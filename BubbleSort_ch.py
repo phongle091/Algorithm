952a1a2eeca4cf8b303314f76e33abc5c4393bb4
phongle091 <phonganhhn.02@gmail.com>
2020-08-25 14:56:19 +0700

    "Not Origin"

---
+++ BubbleSort_ch.py
@@ -0,0 +1,19 @@
+import numpy as np
+import time
+
+
+def BubbleSort(a):
+    n = len(a)
+    for j in range(n-1):
+        for i in range(n-j-1):
+            if a[i] > a[i+1]:
+                (a[i], a[i+1]) = (a[i+1], a[i])
+            # else: continue
+    return a
+
+n = 50000
+a = np.random.randint(500000, size = n)
+start_time = time.time()
+a = BubbleSort(a)
+print(a)
+print("--- sorting cost %s seconds ---" % (time.time() - start_time))
